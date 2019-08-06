a69a4ad0c5e559ef69c3463bfbe7f0dd4b32a913
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2019-08-06 19:51:55 -0400

    Bugfix for health display

--- PlayerStats/PlayerHealth.gd
+++ PlayerStats/PlayerHealth.gd
@@ -14,10 +14,10 @@ func _draw() -> void:
     var on = Rect2(32, 0, 32, 32)
     for i in range(max_health):
         var pos = position + Vector2(i * 32, 0)
-        draw_texture_rect_region(_image, Rect2(pos, Vector2(32, 32)), off if i > health else on)
+        draw_texture_rect_region(_image, Rect2(pos, Vector2(32, 32)), off if i >= health else on)
 
 func set_health(a: int) -> void:
-    health = clamp(a, 0, max_health)
+    health = int(clamp(a, 0, max_health))
     update()
 
 func get_health() -> int:
