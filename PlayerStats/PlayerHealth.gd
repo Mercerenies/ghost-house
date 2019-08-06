53e3e1b9562b4b20b5a26711054c1f735395dce2
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2019-08-06 18:02:25 -0400

    Health is now discrete units

---
+++ PlayerStats/PlayerHealth.gd
@@ -0,0 +1,24 @@
+tool
+extends Node2D
+
+export var max_health: int = 5
+export var health: int = 5
+
+const _image: Texture = preload("res://PlayerStats/Heart.png")
+
+func _ready():
+    pass
+
+func _draw() -> void:
+    var off = Rect2(0, 0, 32, 32)
+    var on = Rect2(32, 0, 32, 32)
+    for i in range(max_health):
+        var pos = position + Vector2(i * 32, 0)
+        draw_texture_rect_region(_image, Rect2(pos, Vector2(32, 32)), off if i > health else on)
+
+func set_health(a: int) -> void:
+    health = clamp(a, 0, max_health)
+    update()
+
+func get_health() -> int:
+    return health
--- PlayerStats/PlayerStats.gd
+++ PlayerStats/PlayerStats.gd
@@ -1,25 +1,22 @@
 extends Node2D
 
-var _playerhealth = 100.0
 var _playerstamina = 100.0
 var _change_per_sec = 100.0
 
 func _ready() -> void:
-    $PlayerHealth.value = _playerhealth
     $PlayerStamina.value = _playerstamina
 
 func _process(delta: float) -> void:
-    $PlayerHealth.value = Util.toward($PlayerHealth.value, _change_per_sec * delta, _playerhealth)
     $PlayerStamina.value = Util.toward($PlayerStamina.value, _change_per_sec * delta, _playerstamina)
 
 func get_player_health() -> float:
-    return _playerhealth
+    return $PlayerHealth.get_health()
 
 func get_player_stamina() -> float:
     return _playerstamina
 
 func set_player_health(a: float) -> void:
-    _playerhealth = clamp(a, 0, 100)
+    $PlayerHealth.set_health(a)
 
 func set_player_stamina(a: float) -> void:
     _playerstamina = clamp(a, 0, 100)
