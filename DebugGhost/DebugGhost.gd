961bc431435f147e8e50b62c8b6d3a22b5e95432
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2019-12-30 19:57:08 -0500

    Fixed outdated code in the debug room

--- DebugGhost/DebugGhost.gd
+++ DebugGhost/DebugGhost.gd
@@ -6,3 +6,6 @@ func _ready():
             {"command": "say", "speaker": "A Ghost", "text": "Hi, I'm a ghost :)"}
         ]
     }
+
+func on_interact() -> void:
+    get_room().show_dialogue(dialogue, "idle")
