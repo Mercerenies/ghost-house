88f688aa38c583e2f0cb8679de61401202b0a899
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2019-12-31 13:45:24 -0500

    FlyingBook AI

--- FlyingBook/FlyingBook.gd
+++ FlyingBook/FlyingBook.gd
@@ -5,16 +5,30 @@ const Player = preload("res://Player/Player.gd")
 const VALID_COLORS = [Color(0xBA7153FF), Color(0x815D83FF), Color(0x945454FF), Color(0x565794FF), Color(0x57816AFF)]
 const INTRO_IMAGE_SPEED = 8
 const IMAGE_SPEED = 8
+const CHARGING_IMAGE_SPEED = 16
+
 const DISAPPEAR_SPEED = 2
 const IDLE_MOVEMENT_SPEED = 10
+const CHARGING_MOVEMENT_SPEED = 210
+const MAX_PLAYER_DISTANCE = 768
+const MIN_CHARGE_DISTANCE = 128
+const MAX_CHARGE_DISTANCE = 256
 
 enum State {
+    # Playing introductory animation
     Introducing,
+    # Quietly following player
     Idle,
+    # Charging for attack
+    Preparing,
+    # Attacking
+    Attacking,
+    # Fading out
     Disappearing,
 }
 
 var state: int = State.Introducing
+var attack_vector = Vector2()
 var anim_index = 0.0
 var hover_index = 0.0
 
@@ -30,14 +44,46 @@ func _process(delta: float) -> void:
             anim_index += INTRO_IMAGE_SPEED * delta
             if anim_index >= 4:
                 state = State.Idle
+
         State.Idle:
             hover_index += delta
             anim_index += IMAGE_SPEED * delta
             anim_index = (fmod((anim_index - 4), 4)) + 4
             $Sprite.position.y = 10 + 4 * sin(hover_index * 2 * PI)
-            var player = get_room().get_marked_entities()['player']
-            position += (player.global_position - global_position).normalized() * IDLE_MOVEMENT_SPEED * delta
+
+            var player = EnemyAI.get_player(self)
+            var player_dist = EnemyAI.distance_to_player(self)
+            var player_dir = EnemyAI.player_line_of_sight(self)
+
+            if player_dist > MAX_PLAYER_DISTANCE:
+                state = State.Disappearing
+            if player_dir > 3 * PI / 8:
+                position += (player.global_position - global_position).normalized() * IDLE_MOVEMENT_SPEED * delta
+
+        State.Preparing:
+            hover_index += 2 * delta
+            anim_index += CHARGING_IMAGE_SPEED * delta
+            anim_index = (fmod((anim_index - 4), 4)) + 4
+            $Sprite.position.y = 10 + 4 * sin(hover_index * 2 * PI)
+
+        State.Attacking:
+            hover_index += 2 * delta
+            anim_index += CHARGING_IMAGE_SPEED * delta
+            anim_index = (fmod((anim_index - 4), 4)) + 4
+            $Sprite.position.y = 10 + 4 * sin(hover_index * 2 * PI)
+            position += attack_vector * CHARGING_MOVEMENT_SPEED * delta
+
         State.Disappearing:
+            hover_index += delta
+            anim_index += IMAGE_SPEED * delta
+            anim_index = (fmod((anim_index - 4), 4)) + 4
+            $Sprite.position.y = 10 + 4 * sin(hover_index * 2 * PI)
+
+            # If the attack vector is zero, this does nothing. If it's
+            # nonzero, then continue the "attack" (for the sake of fluid
+            # animation)
+            position += attack_vector * CHARGING_MOVEMENT_SPEED * delta
+
             modulate.a = Util.toward(modulate.a, delta * DISAPPEAR_SPEED, 0)
             if modulate.a == 0:
                 queue_free()
@@ -46,9 +92,33 @@ func _process(delta: float) -> void:
 
 
 func _on_Area2D_area_entered(area):
-    if state == State.Idle:
+    if state != State.Introducing and state != State.Disappearing:
         if area.get_parent() is Player:
             var stats = get_room().get_player_stats()
             if stats.damage_player(1):
                 state = State.Disappearing
-                
+
+
+func _on_StateTimer_timeout():
+    match state:
+        State.Introducing:
+            pass
+        State.Idle:
+            var dist = EnemyAI.distance_to_player(self)
+            var dir = EnemyAI.player_line_of_sight(self)
+            if dist >= MIN_CHARGE_DISTANCE and dist < MAX_CHARGE_DISTANCE and dir > 3 * PI / 8:
+                if randf() < 0.75:
+                    state = State.Preparing
+                    $StateTimer.start(randf() + 1)
+        State.Preparing:
+            var player = EnemyAI.get_player(self)
+            var dir = EnemyAI.player_line_of_sight(self)
+            if dir > 3 * PI / 8:
+                attack_vector = (player.position + Vector2(16, 16) - self.position).normalized()
+                state = State.Attacking
+                $StateTimer.start(1.5)
+        State.Attacking:
+            state = State.Disappearing
+            $StateTimer.stop()
+        State.Disappearing:
+            pass
