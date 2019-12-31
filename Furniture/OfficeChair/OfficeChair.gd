dc009b02e0b748364f9acafcefe813cf7313307d
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2019-12-31 17:10:29 -0500

    The two basic chair types now have "Flying" variants

--- Furniture/OfficeChair/OfficeChair.gd
+++ Furniture/OfficeChair/OfficeChair.gd
@@ -4,7 +4,17 @@ func _ready() -> void:
     interaction["idle"] = [
          { "command": "say", "text": "An old office chair." }
     ]
+    vars["flying_furniture"] = false
+    $FlyingFurnitureSpawner.set_entity(self)
+    $FlyingFurnitureSpawner.set_sprite($Sprite)
 
 func set_direction(a: int):
     $Sprite.frame = (5 - a) % 4
 
+func turn_evil() -> void:
+    # 60% chance of launching self, 40% chance of being vanishing
+    if randf() < 0.6:
+        vars['flying_furniture'] = true
+        $FlyingFurnitureSpawner.activate()
+    else:
+        .turn_evil()
