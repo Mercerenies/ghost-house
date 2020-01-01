3972bd66f800a1180bd60228a1be009d945b31c0
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2020-01-01 12:36:07 -0500

    Recliners fly now

--- Furniture/Recliner/Recliner.gd
+++ Furniture/Recliner/Recliner.gd
@@ -1,10 +1,22 @@
 extends Furniture
 
+func _init() -> void:
+    vars["flying_furniture"] = false
+
 func _ready() -> void:
     interaction["idle"] = [
          { "command": "say", "text": "A comfy-looking recliner." }
     ]
+    $FlyingFurnitureSpawner.set_entity(self)
+    $FlyingFurnitureSpawner.set_sprite($Sprite)
 
 func set_direction(a: int):
     $Sprite.frame = (5 - a) % 4
 
+func turn_evil() -> void:
+    # 60% chance of launching self, 40% chance of being vanishing
+    if randf() < 0.6:
+        vars['flying_furniture'] = true
+        $FlyingFurnitureSpawner.activate()
+    else:
+        .turn_evil()
