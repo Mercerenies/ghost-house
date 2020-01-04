abf2775e3cfbaa7584a8c3d77f6d7b89cd9d3921
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2020-01-03 20:08:13 -0500

    Temporarily increased enemy spawn rate for testing purposes

--- DebugGeneratedRoom/DebugGeneratedRoom.gd
+++ DebugGeneratedRoom/DebugGeneratedRoom.gd
@@ -4,7 +4,8 @@ const Generator = preload("res://Generator/Generator.gd")
 
 func _ready():
     randomize()
-    var gen = Generator.new({ 'config': { 'width': 12, 'height': 12, "percent_evil": 0.05 } })
+	# DEBUG CODE (should be 0.05 percent_evil)
+    var gen = Generator.new({ 'config': { 'width': 12, 'height': 12, "percent_evil": 0.50 } })
     var room = gen.generate()
     gen.free()
     add_child(room)
