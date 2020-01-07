d1ac5d2bd65e6cacd30fd1bc8ff68f9264f638ec
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2020-01-07 17:42:15 -0500

    WeightedDistr helper

--- RoomTypes/RoomTypes.gd
+++ RoomTypes/RoomTypes.gd
@@ -47,12 +47,30 @@ enum RT {
     Garage
 }
 
+# TODO Make UniformDistr and WeightedDistr return managers themselves
+# so they can be composed with other managers.
+
 class UniformDistr extends SpecialPlacementManager:
     var _values: Array
     func _init(values: Array) -> void:
         _values = values
     func determine_placements() -> Array:
         return _values[randi() % len(_values)]
+
+class WeightedDistr extends SpecialPlacementManager:
+    var _values: Array
+    func _init(values: Array) -> void:
+        _values = values
+    func determine_placements() -> Array:
+        var total = 0
+        for v in _values:
+            total += v["weight"]
+        var choice = randi() % int(total)
+        for v in _values:
+            choice -= v["weight"]
+            if choice < 0:
+                return v["result"]
+        return [] # This shouldn't happen
 
 var walls: Array = [Tile.DebugWall, Tile.StripedWall1, Tile.StripedWall2, Tile.DiamondWall, Tile.CircleWall,
                     Tile.GradientWall1, Tile.GradientWall2, Tile.RockyWall, Tile.PipeWall, Tile.RedRegalWall,
