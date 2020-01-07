90f69bf20fca4843ed0e408f3d14f9185a650c8a
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2020-01-06 20:01:37 -0500

    Removed lots of now-unnecessary functions

--- Generator/Generator.gd
+++ Generator/Generator.gd
@@ -51,191 +51,6 @@ const TOTAL_CELL_SIZE = GeneratorData.TOTAL_CELL_SIZE
 func _init(room_data: Dictionary):
     _data = room_data
 
-func _add_entity(pos: Vector2, entity: Object) -> void:
-    _room.get_node("Entities").add_child(entity)
-    entity.position = pos * 32
-    entity.position_self()
-    if entity is Player:
-        _room.get_marked_entities()["player"] = entity
-
-func _paint_room(id: int, rect: Rect2) -> void:
-    for x in range(rect.position.x, rect.end.x):
-        for y in range(rect.position.y, rect.end.y):
-            _grid.set_value(Vector2(x, y), id)
-    _boxes[id] = RoomData.new(id, rect)
-
-func _open_all_doorways() -> void:
-    # This code literally only exists for debugging purposes.
-    var w = _data['config']['width']
-    var h = _data['config']['height']
-    for x in range(w):
-        for y in range(h):
-            var xpos = x * TOTAL_CELL_SIZE + WALL_SIZE
-            var ypos = y * TOTAL_CELL_SIZE + WALL_SIZE
-            _room.set_tile_cell(Vector2(xpos - 1, ypos + 1), _room.Tile.DebugFloor)
-            _room.set_tile_cell(Vector2(xpos - 1, ypos + 2), _room.Tile.DebugFloor)
-            _room.set_tile_cell(Vector2(xpos + 1, ypos - 1), _room.Tile.DebugFloor)
-            _room.set_tile_cell(Vector2(xpos + 2, ypos - 1), _room.Tile.DebugFloor)
-            _room.set_tile_cell(Vector2(xpos + 4, ypos + 1), _room.Tile.DebugFloor)
-            _room.set_tile_cell(Vector2(xpos + 4, ypos + 2), _room.Tile.DebugFloor)
-            _room.set_tile_cell(Vector2(xpos + 1, ypos + 4), _room.Tile.DebugFloor)
-            _room.set_tile_cell(Vector2(xpos + 2, ypos + 4), _room.Tile.DebugFloor)
-
-func is_blocked(pos: Vector2) -> bool:
-    return _room.is_wall_at(pos) or _room.get_entity_cell(pos) != null
-
-func _can_put_furniture_at(rect: Rect2) -> bool:
-    # Check the position we want to put it at first
-    for i in range(rect.size.x):
-        for j in range(rect.size.y):
-            if is_blocked(rect.position + Vector2(i, j)):
-                return false
-    var transitions = 0
-    # Top edge
-    for i in range(rect.size.x + 1):
-        var pos = Vector2(rect.position.x - 1 + i, rect.position.y - 1)
-        if is_blocked(pos) != is_blocked(pos + Vector2(1, 0)):
-            transitions += 1
-    # Right edge
-    for i in range(rect.size.y + 1):
-        var pos = Vector2(rect.end.x, rect.position.y - 1 + i)
-        if is_blocked(pos) != is_blocked(pos + Vector2(0, 1)):
-            transitions += 1
-    # Bottom edge
-    for i in range(rect.size.x + 1):
-        var pos = Vector2(rect.end.x - i, rect.end.y)
-        if is_blocked(pos) != is_blocked(pos + Vector2(-1, 0)):
-            transitions += 1
-    # Left edge
-    for i in range(rect.size.y + 1):
-        var pos = Vector2(rect.position.x - 1, rect.end.y - i)
-        if is_blocked(pos) != is_blocked(pos + Vector2(0, -1)):
-            transitions += 1
-    return transitions <= 2
-
-func _is_doorway_at_position(pos: Vector2) -> bool:
-    if not _room.is_wall_at(pos):
-        # Left edge
-        var cell = Vector2(floor(pos.x / TOTAL_CELL_SIZE), floor(pos.y / TOTAL_CELL_SIZE))
-        if int(pos.x) % TOTAL_CELL_SIZE == 0:
-            if _grid.get_value(cell) != _grid.get_value(cell + Vector2(-1, 0)):
-                return true
-        # Right edge
-        if int(pos.x) % TOTAL_CELL_SIZE == TOTAL_CELL_SIZE - 1:
-            if _grid.get_value(cell) != _grid.get_value(cell + Vector2(1, 0)):
-                return true
-        # Top Edge
-        if int(pos.y) % TOTAL_CELL_SIZE == 0:
-            if _grid.get_value(cell) != _grid.get_value(cell + Vector2(0, -1)):
-                return true
-        # Bottom Edge
-        if int(pos.y) % TOTAL_CELL_SIZE == TOTAL_CELL_SIZE - 1:
-            if _grid.get_value(cell) != _grid.get_value(cell + Vector2(0, 1)):
-                return true
-    return false
-
-func _is_blocking_doorway(rect: Rect2) -> bool:
-    for i in range(rect.size.x + 2):
-        for j in range(rect.size.y + 2):
-            # Cut the corners
-            if (i == 0 or i == rect.size.x + 1) and (j == 0 or j == rect.size.y + 1):
-                continue
-            # Perform the check
-            if _is_doorway_at_position(rect.position + Vector2(i - 1, j - 1)):
-                return true
-    return false
-
-const tmp = preload("res://Furniture/DebugLittleGreenBox/DebugLittleGreenBox.tscn")
-const tmp_TwinBedPlacement = preload("res://Furniture/TwinBed/TwinBedPlacement.gd")
-const tmp_KingBedPlacement = preload("res://Furniture/KingBed/KingBedPlacement.gd")
-const tmp_EdgePlacementManager = preload("res://Furniture/EdgePlacementManager.gd")
-const tmp_EdgeBookshelfPlacement = preload("res://Furniture/Bookshelf/EdgeBookshelfPlacement.gd")
-const tmp_EdgeLongBookshelfPlacement = preload("res://Furniture/LongBookshelf/EdgeLongBookshelfPlacement.gd")
-
-# func _debug_furniture_flood() -> void:
-#     var w = _data['config']['width']
-#     var h = _data['config']['height']
-#     for i in range(w * TOTAL_CELL_SIZE):
-#         for j in range(h * TOTAL_CELL_SIZE):
-#             if _can_put_furniture_at(Rect2(i, j, 2, 2)) and not _is_blocking_doorway(Rect2(i, j, 2, 2)):
-#                 var silly = tmp.instance()
-#                 _add_entity(Vector2(i, j), silly)
-
-func _consider_turning_evil(obj) -> void:
-    var chance = _data['config']['percent_evil'] * obj.chance_of_turning_evil()
-    if randf() < chance:
-        obj.turn_evil()
-
-func _try_to_place(room, placement) -> void:
-    var positions = placement.enumerate(room)
-    var valid_positions = []
-    for value in positions:
-        var pos = placement.value_to_position(value)
-        if _can_put_furniture_at(pos):
-            if placement.can_block_doorways() or not _is_blocking_doorway(pos):
-                valid_positions.append(value)
-    if len(valid_positions) == 0:
-        return
-    var chosen = valid_positions[randi() % len(valid_positions)]
-    var obj = placement.spawn_at(chosen)
-    if obj != null:
-        _add_entity(placement.value_to_position(chosen).position, obj)
-        _consider_turning_evil(obj)
-
-func _debug_edges() -> void:
-    var w = _data['config']['width'] * TOTAL_CELL_SIZE
-    var h = _data['config']['height'] * TOTAL_CELL_SIZE
-    for i in range(w):
-        for j in range(h):
-            if _flag_grid.get_flag(Vector2(i, j), FLAG_EDGE_FURNITURE) and _can_put_furniture_at(Rect2(i, j, 1, 1)) and not _is_blocking_doorway(Rect2(i, j, 1, 1)):
-                 var silly = tmp.instance()
-                 _add_entity(Vector2(i, j), silly)
-
-func _fill_special() -> void:
-    for k in _boxes:
-        var v = _boxes[k]
-        for spec in v.specialtype:
-            _try_to_place(v, spec)
-
-func _fill_edges() -> void:
-    var w = _data['config']['width'] * TOTAL_CELL_SIZE
-    var h = _data['config']['height'] * TOTAL_CELL_SIZE
-    for x in range(w):
-        for y in range(h):
-            var room = _boxes[_grid.get_value(Vector2(floor(x / TOTAL_CELL_SIZE), floor(y / TOTAL_CELL_SIZE)))]
-            var mngr = room.edgetype
-            if _flag_grid.get_flag(Vector2(x, y), FLAG_EDGE_FURNITURE):
-                var direction = -1
-                if _room.is_wall_at(Vector2(x - 1, y)) and not is_blocked(Vector2(x + 1, y)) and not _flag_grid.get_flag(Vector2(x + 1, y), FLAG_EDGE_FURNITURE):
-                    direction = 0
-                elif _room.is_wall_at(Vector2(x, y - 1)) and not is_blocked(Vector2(x, y + 1)) and not _flag_grid.get_flag(Vector2(x, y + 1), FLAG_EDGE_FURNITURE):
-                    direction = 1
-                elif _room.is_wall_at(Vector2(x + 1, y)) and not is_blocked(Vector2(x - 1, y)) and not _flag_grid.get_flag(Vector2(x - 1, y), FLAG_EDGE_FURNITURE):
-                    direction = 2
-                elif _room.is_wall_at(Vector2(x, y + 1)) and not is_blocked(Vector2(x, y - 1)) and not _flag_grid.get_flag(Vector2(x, y - 1), FLAG_EDGE_FURNITURE):
-                    direction = 3
-                else:
-                    continue
-                var max_width = 1
-                if direction % 2 == 0:
-                    # Vertical expand
-                    while _flag_grid.get_flag(Vector2(x, y + max_width), FLAG_EDGE_FURNITURE):
-                        max_width += 1
-                else:
-                    # Horizontal expand
-                    while _flag_grid.get_flag(Vector2(x + max_width, y), FLAG_EDGE_FURNITURE):
-                        max_width += 1
-                var arr = mngr.generate_at_position(Vector2(x, y), direction, max_width)
-                if not (arr is Array):
-                    arr = [arr]
-                for obj in arr:
-                    if obj != null:
-                        var pos = Vector2(floor(obj.position.x / 32), floor(obj.position.y / 32))
-                        var rect = Rect2(pos, obj.dims)
-                        if _can_put_furniture_at(rect) and not _is_blocking_doorway(rect):
-                            _add_entity(pos, obj)
-                            _consider_turning_evil(obj)
-
 func generate() -> Room:
     var w = _data['config']['width']
     var h = _data['config']['height']
@@ -268,21 +83,17 @@ func generate() -> Room:
 
     _room.get_minimap().initialize(Vector2(w, h), _grid, _boxes, _connections)
 
-    #_debug_edges()
-    #for i in range(_data['config']['width']):
-    #    for j in range(_data['config']['height']):
-    #        _room.set_tile_cell(Vector2(i, j), _room.Tile.DebugFloor)
     var player = PlayerScene.instance()
     # DEBUG CODE We'll clean this up later; it's quick and dirty player placement right now
     var playercheck = false
     for i in range(100):
         for j in range(100):
             if _room.get_entity_cell(Vector2(i + 2, j + 2)) == null:
-                _add_entity(Vector2(i + 2, j + 2), player)
+                helper.add_entity(Vector2(i + 2, j + 2), player)
                 playercheck = true
                 break
         if playercheck:
             break
     player.connect("player_moved", _room.get_minimap(), "update_map")
-    #_debug_furniture_flood()
+
     return _room
