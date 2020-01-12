0c60cdd7e38906b8d6a09d4bf953ddfa6595f371
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2020-01-12 13:32:22 -0500

    Second type of car

--- Furniture/Automobile/Automobile.gd
+++ Furniture/Automobile/Automobile.gd
@@ -1,12 +1,16 @@
 extends Furniture
 
+var _image_type: int = 0
+
 func _ready() -> void:
     interaction["idle"] = [
          { "command": "say", "text": "An old-fashioned automobile." }
     ]
+    _image_type = randi() % 2
+    $Sprite.frame = 4 * _image_type + ($Sprite.frame % 4)
 
 func set_direction(a: int):
-    $Sprite.frame = a
+    $Sprite.frame = 4 * _image_type + a
 
 func get_furniture_name():
     return "Automobile"
