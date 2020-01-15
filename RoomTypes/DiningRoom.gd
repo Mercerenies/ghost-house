01e0b0192b3b22bd96cee66b330bc27c6ed72b54
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2020-01-15 13:55:34 -0500

    Dining room refinements

--- RoomTypes/DiningRoom.gd
+++ RoomTypes/DiningRoom.gd
@@ -81,7 +81,7 @@ class SurroundedInChairs extends FurniturePlacement:
             if randf() < _rate:
                 cb.call(_Helper._make_chair_facing(upperleft + Vector2(bounds.size.x + 1, i + 1), 2))
 
-class ChaoticTables extends FurniturePlacement:
+class ScatteredTablePlacement extends FurniturePlacement:
 
     func enumerate(_room) -> Array:
         return [0]
@@ -89,64 +89,159 @@ class ChaoticTables extends FurniturePlacement:
     func value_to_position(_value) -> Rect2:
         return GeneratorData.PLACEMENT_SAFE
 
-    func spawn_at(room, value, cb):
-        # ///// Refine me then make some variants
+    func placement_object() -> FurniturePlacement:
+        return null
+
+    func count(bounds: Vector2) -> int:
+        var max_dim = max(bounds.x, bounds.y)
+        return Util.randi_range(max_dim, max_dim + 2)
+
+    func spawn_at(room, _value, cb):
         var box = room.box
         var cells = Rect2(box.position * TOTAL_CELL_SIZE, box.size * TOTAL_CELL_SIZE)
         cells.position += Vector2(WALL_SIZE, WALL_SIZE)
         cells.size -= 2 * Vector2(WALL_SIZE, WALL_SIZE)
 
-        for _i in range(10):
-            var curr
-            match randi() % 9:
-                0, 1:
-                    curr = CircularDiningTablePlacement.new()
-                2, 3:
-                    curr = DiningTablePlacement.new(Vector2(2, 2))
-                4, 5:
-                    curr = DiningTablePlacement.new(Vector2(3, 2))
-                6, 7:
-                    curr = DiningTablePlacement.new(Vector2(2, 3))
-                8:
-                    curr = DiningTablePlacement.new(Vector2(3, 3))
-            if randf() < 0.9:
-                var rate = Util.choose([0.7, 0.8, 0.9, 1.0])
-                curr = SurroundedInChairs.new(curr, rate)
+        var count = count(box.size)
+
+        for _i in range(count):
+            var curr = placement_object()
+            if curr == null:
+                continue
             var pos = Vector2(Util.randi_range(cells.position.x, cells.end.x),
                               Util.randi_range(cells.position.y, cells.end.y))
-            curr = curr.spawn_at(room, { "position": pos }, cb)
-            if not (curr is Array):
-                curr = [curr]
-            for v in curr:
-                cb.call(v)
+            curr.spawn_at(room, { "position": pos }, cb)
+
+class ChaoticTables extends ScatteredTablePlacement:
+
+    func placement_object() -> FurniturePlacement:
+        var curr = null
+        match randi() % 9:
+            0, 1:
+                curr = CircularDiningTablePlacement.new()
+            2, 3:
+                curr = DiningTablePlacement.new(Vector2(2, 2))
+            4, 5:
+                curr = DiningTablePlacement.new(Vector2(3, 2))
+            6, 7:
+                curr = DiningTablePlacement.new(Vector2(2, 3))
+            8:
+                curr = DiningTablePlacement.new(Vector2(3, 3))
+        if randf() < 0.9:
+            var rate = Util.choose([0.7, 0.8, 0.9, 1.0])
+            curr = SurroundedInChairs.new(curr, rate)
+        return curr
+
+class UniformTables extends ScatteredTablePlacement:
+    var _type
+
+    func _init(type=null):
+        if type == null:
+            type = Util.choose([CircularDiningTablePlacement.new(),
+                                DiningTablePlacement.new(Vector2(2, 2))])
+        _type = type
+
+    func placement_object() -> FurniturePlacement:
+        var curr = _type
+        if randf() < 0.9:
+            var rate = Util.choose([0.7, 0.8, 0.9, 1.0])
+            curr = SurroundedInChairs.new(curr, rate)
+        return curr
+
+class UniformTablesWithChairs extends ScatteredTablePlacement:
+    var _type
+
+    func _init(type=null):
+        if type == null:
+            type = Util.choose([CircularDiningTablePlacement.new(),
+                                DiningTablePlacement.new(Vector2(2, 2))])
+        type = SurroundedInChairs.new(type, 1.0)
+        _type = type
+
+    func placement_object() -> FurniturePlacement:
+        return _type
+
+class UniformTablesWithoutChairs extends ScatteredTablePlacement:
+    var _type
+
+    func _init(type=null):
+        if type == null:
+            type = Util.choose([CircularDiningTablePlacement.new(),
+                                DiningTablePlacement.new(Vector2(2, 2)),
+                                DiningTablePlacement.new(Vector2(2, 3)),
+                                DiningTablePlacement.new(Vector2(3, 2))])
+        _type = type
+
+    func count(bounds: Vector2) -> int:
+        return .count(bounds) + 2
+
+    func placement_object() -> FurniturePlacement:
+        return _type
+
+class TableGrid extends FurniturePlacement:
+    var _type
+
+    func _init(type=null):
+        if type == null:
+            type = Util.choose([CircularDiningTablePlacement.new(),
+                                DiningTablePlacement.new(Vector2(2, 2))])
+        _type = type
+
+    func enumerate(_room) -> Array:
+        return [0]
+
+    func value_to_position(_value) -> Rect2:
+        return GeneratorData.PLACEMENT_SAFE
+
+    func spawn_at(room, _value, cb):
+        var placement = SurroundedInChairs.new(_type, 1.0)
+
+        var box = room.box
+        var cells = Rect2(box.position * TOTAL_CELL_SIZE, box.size * TOTAL_CELL_SIZE)
+        cells.position += Vector2(WALL_SIZE, WALL_SIZE)
+        cells.size -= 2 * Vector2(WALL_SIZE, WALL_SIZE)
+
+        var start = cells.position
+        start += Vector2(randi() % 3, randi() % 3)
+
+        for i in range(start.x, cells.end.x, 6):
+            for j in range(start.y, cells.end.y, 6):
+                placement.spawn_at(room, { "position": Vector2(i, j) }, cb)
 
 class PlacementManager extends SpecialPlacementManager:
-
-    func _chaotic_tables(min_count: int, max_count: int) -> Array:
-        # "Chaotic Tables" Layout
-        var count = Util.randi_range(min_count, max_count + 1)
-        var arr = []
-        for _i in range(count):
-            var curr
-            match randi() % 9:
-                0, 1:
-                    curr = CircularDiningTablePlacement.new()
-                2, 3:
-                    curr = DiningTablePlacement.new(Vector2(2, 2))
-                4, 5:
-                    curr = DiningTablePlacement.new(Vector2(3, 2))
-                6, 7:
-                    curr = DiningTablePlacement.new(Vector2(2, 3))
-                8:
-                    curr = DiningTablePlacement.new(Vector2(3, 3))
-            if randf() < 0.9:
-                var rate = Util.choose([0.7, 0.8, 0.9, 1.0])
-                curr = SurroundedInChairs.new(curr, rate)
-            arr.append(curr)
-        return arr
 
     func determine_placements(size: Vector2) -> Array:
         var min_dim = min(size.x, size.y)
         var max_dim = max(size.x, size.y)
 
-        return [ChaoticTables.new()] # _chaotic_tables(2 * max_dim, 2 * max_dim + 2)
+        if max_dim >= 3 and randf() < 0.1:
+            return [TableGrid.new()]
+        if min_dim >= 3 and randf() < 0.3:
+            return [TableGrid.new()]
+
+        var chair_type
+        match randi() % 13:
+            0, 1, 2:
+                chair_type = CircularDiningTablePlacement.new()
+            3, 4, 5:
+                chair_type = DiningTablePlacement.new(Vector2(2, 2))
+            6, 7:
+                chair_type = DiningTablePlacement.new(Vector2(2, 3))
+            8, 9:
+                chair_type = DiningTablePlacement.new(Vector2(3, 2))
+            10:
+                chair_type = DiningTablePlacement.new(Vector2(3, 3))
+            11:
+                chair_type = DiningTablePlacement.new(Vector2(4, 2))
+            12:
+                chair_type = DiningTablePlacement.new(Vector2(2, 4))
+
+        match randi() % 4:
+            0:
+                return [ChaoticTables.new()]
+            1:
+                return [UniformTables.new(chair_type)]
+            2:
+                return [UniformTablesWithChairs.new(chair_type)]
+            3:
+                return [UniformTablesWithoutChairs.new(chair_type)]
