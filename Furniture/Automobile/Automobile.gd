c26e55112a3b318925ea0a8feb6ec85b0621006f
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2020-01-18 17:40:14 -0500

    Fixed some problems with setting the car's direction

--- Furniture/Automobile/Automobile.gd
+++ Furniture/Automobile/Automobile.gd
@@ -1,6 +1,7 @@
 extends Furniture
 
 var _image_type: int = -1
+var _dir: int = 0
 
 func _ready() -> void:
     interaction["idle"] = [
@@ -8,13 +9,14 @@ func _ready() -> void:
     ]
     if _image_type == -1:
         _image_type = randi() % $Sprite.vframes
-    $Sprite.frame = 4 * _image_type + ($Sprite.frame % 4)
+    $Sprite.frame = 4 * _image_type + _dir
 
 func set_image_type(a: int):
     _image_type = a
 
 func set_direction(a: int):
     $Sprite.frame = 4 * _image_type + a
+    _dir = a
 
 func get_furniture_name():
     return "Automobile"
