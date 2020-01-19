2e9c56d3f904af54b799eda45806a07f43873707
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2020-01-19 15:09:23 -0500

    (Full) Laundry baskets can now spawn flying clothes

--- Furniture/LaundryBasket/LaundryBasket.gd
+++ Furniture/LaundryBasket/LaundryBasket.gd
@@ -1,14 +1,17 @@
 extends Furniture
 
+var _image_index: int
+
 func _init() -> void:
     vars["flying_clothes"] = false
+    _image_index = randi() % 2
 
 func _ready() -> void:
     interaction["idle"] = [
          { "command": "say", "text": "A basket for laundry." }
     ]
     $FlyingClothesSpawner.set_entity(self)
-    $Sprite.frame = randi() % 2
+    $Sprite.frame = _image_index
 
 func set_direction(a: int):
     pass
@@ -21,5 +24,8 @@ func turn_evil() -> void:
     if randf() < 0.5:
         vars['flying_clothes'] = true
         $FlyingClothesSpawner.activate()
+        _image_index = 1
+        if $Sprite:
+            $Sprite.frame = 1
     else:
         .turn_evil()
