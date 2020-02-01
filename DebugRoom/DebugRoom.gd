38fae04c446ab446730af4bee1b0d1dcd7dc4e6c
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2020-01-31 21:04:29 -0500

    Minor touchups to dialogue branching

--- DebugRoom/DebugRoom.gd
+++ DebugRoom/DebugRoom.gd
@@ -5,15 +5,15 @@ extends Node2D
 var _sample_dialogue = {
     "idle": [
         {"command": "say", "text": "Hi, I'm your friendly neighborhood dialogue box. I'm here to help. Press SPACEBAR and stuff will happen."},
-#        {"command": "say", "text": "You did it! You defeated me!"},
-#        {"command": "branch", "speaker": "Box", "text": "Are you done?", "options": [
-#            { "text": "No, continue.", "state": "state2" },
-#            { "text": "Yes, done.", "state": "end" },
-#            { "text": "Say again.", "state": "start" },
-#            { "text": "Say again 1.", "state": "start" },
-#            { "text": "Say again 2.", "state": "start" },
-#            { "text": "Say again 3.", "state": "start" }
-#        ]}
+        {"command": "say", "text": "You did it! You defeated me!"},
+        {"command": "branch", "speaker": "Box", "text": "Are you done?", "options": [
+            { "text": "No, continue.", "state": "state2" },
+            { "text": "Yes, done.", "state": "end" },
+            { "text": "Say again.", "state": "idle" },
+            { "text": "Say again 1.", "state": "idle" },
+            { "text": "Say again 2.", "state": "idle" },
+            { "text": "Say again 3.", "state": "idle" }
+        ]},
         {"command": "end"}
     ],
     "state2": [
@@ -26,7 +26,7 @@ var _sample_dialogue = {
 }
 
 func _ready():
-    #$Room.get_dialogue_box().popup(_sample_dialogue, "idle")
+    $Room.get_dialogue_box().popup(_sample_dialogue, "idle")
     $Room.get_marked_entities()["player"] = $Room/Entities/Player
     $Room/Entities/LongBookshelf.turn_evil()
     $Room/Entities/OfficeChair.turn_evil()
--- DialogueBox/Branching.gd
+++ DialogueBox/Branching.gd
@@ -13,14 +13,16 @@ func _update_self():
     $Label.text = ""
     for opt in _options:
         $Label.text += opt['text'] + '\n';
-    call_deferred("_update_text")
+    call_deferred("_update_text") # Need to give the label a frame to update itself.
 
 func _update_text():
+
     var height = ($Label.get_line_height()) * ($Label.get_line_count() - 1)
     $Label.rect_position.y = _base_frame_y - height + 4
     $Frame.polygon[0].y = _base_frame_y - (height + 4)
     $Frame.polygon[1].y = _base_frame_y - (height + 4)
     $Frame.polygon[4].y = _base_frame_y - (height + 4)
+
     $CurrentOption.position = $Label.rect_position + Vector2(-16, $Label.get_line_height() / 2)
     var line_height = $Label.rect_size.y / (len(_options) + 1)
     $CurrentOption.position.y += line_height * _option
@@ -47,4 +49,3 @@ func _process(_delta: float) -> void:
             _option += dir
             _option = (_option % len(_options) + len(_options)) % len(_options)
             _update_self()
-    _update_text()
