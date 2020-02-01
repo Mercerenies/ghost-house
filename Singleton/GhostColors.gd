0902d2cfccac59f6a494fec3ad63a99a278db7a4
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2020-02-01 17:53:25 -0500

    GhostColors

---
+++ Singleton/GhostColors.gd
@@ -0,0 +1,36 @@
+extends Node
+
+enum GhostColor {
+    Red, Green, Blue, Yellow, Pink
+}
+
+func color_constant_to_color(value: int) -> Color:
+    match value:
+        GhostColor.Red:
+            return Color(0xFF7777FF)
+        GhostColor.Green:
+            return Color(0x48B744FF)
+        GhostColor.Blue:
+            return Color(0x4B7FD8FF)
+        GhostColor.Yellow:
+            return Color(0xEBFC21FF)
+        GhostColor.Pink:
+            return Color(0xF946C8FF)
+    return Color()
+
+func color_constant_to_string(value: int) -> String:
+    match value:
+        GhostColor.Red:
+            return "Red"
+        GhostColor.Green:
+            return "Green"
+        GhostColor.Blue:
+            return "Blue"
+        GhostColor.Yellow:
+            return "Yellow"
+        GhostColor.Pink:
+            return "Pink"
+    return ""
+
+func color_constants() -> Array:
+    return [GhostColor.Red, GhostColor.Green, GhostColor.Blue, GhostColor.Yellow, GhostColor.Pink]
