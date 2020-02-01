4c820d6e7fec439600a78a0830d7a7eb1d46831a
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2020-02-01 13:27:58 -0500

    Fix for pause menu height

--- PauseMenu/TopLevelPauseMenu.gd
+++ PauseMenu/TopLevelPauseMenu.gd
@@ -13,13 +13,15 @@ func _update_self():
 
 func _update_text():
 
-    var height = ($Label.get_line_height()) * ($Label.get_line_count() - 1)
-    $Frame.polygon[2].y = $Frame.polygon[1].y + height + 20
-    $Frame.polygon[3].y = $Frame.polygon[0].y + height + 20
+    var height0 = ($Label.get_line_height()) * ($Label.get_line_count() - 1)
+    $Label.rect_size.y = height0 - 4
+    var line_height = $Label.rect_size.y / (len(_options) + 1)
+    var height = $Label.rect_size.y
+
+    $Frame.polygon[2].y = $Frame.polygon[1].y + height + 8
+    $Frame.polygon[3].y = $Frame.polygon[0].y + height + 8
 
     $CurrentOption.position = $Label.rect_position + Vector2(-16, $Label.get_line_height() / 2)
-    $Label.rect_size.y = height - 4
-    var line_height = $Label.rect_size.y / (len(_options) + 1)
     $CurrentOption.position.y += line_height * _option
 
 func set_options(options: Array) -> void:
