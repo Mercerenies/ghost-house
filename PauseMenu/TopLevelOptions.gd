c8b2fefd04093e0456dac729ab0fe475149c8868
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2020-01-31 21:07:21 -0500

    "Back to Game" option

--- PauseMenu/TopLevelOptions.gd
+++ PauseMenu/TopLevelOptions.gd
@@ -1,6 +1,8 @@
 extends Node
 
-const TOP_LEVEL_OPTIONS = ["Example Option 1", "Example Option 2", "Example Option 3"]
+const TOP_LEVEL_OPTIONS = ["Example Option 1", "Example Option 2", "Example Option 3", "Back to Game"]
 
 func _on_TopLevelPauseMenu_option_selected(option: String):
-    print(option)
+    match option:
+        "Back to Game":
+            get_parent().unpause()
