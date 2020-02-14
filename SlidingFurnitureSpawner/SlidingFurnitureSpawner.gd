03ad96e156415cda246b91ac858d80f6d2e8bf11
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2020-02-13 22:11:09 -0500

    Note on sliding furniture

--- SlidingFurnitureSpawner/SlidingFurnitureSpawner.gd
+++ SlidingFurnitureSpawner/SlidingFurnitureSpawner.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+# ///// Need to think about the infrastructure for this, because it's
+# different from FlyingFurniture in a nontrivial way (namely, it needs
+# to be able to position itself in the grid when not moving)
+
 const SlidingFurniture = preload("res://SlidingFurniture/SlidingFurniture.tscn")
 
 const RESPAWN_DISTANCE: float = 512.0
