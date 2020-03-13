32cbd5f69167dd30dde44d69fb5b45711fc74979
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2020-03-13 15:54:26 -0400

    Max inventory limits

--- PlayerStats/PlayerInventory.gd
+++ PlayerStats/PlayerInventory.gd
@@ -4,7 +4,10 @@ extends Node
 # draw here, but since other player data (health, stamina, status
 # effects, etc.) are stored here, might as well put it here as well.
 
+const DEFAULT_MAX_INV_SIZE = 20
+
 var _items: Array = []
+var _max_items: int = DEFAULT_MAX_INV_SIZE
 
 # Like with PlayerStatusEffects, consider this list pseudo-immutable.
 # That is, you may read from the list and mutate specific elements of
@@ -13,10 +16,17 @@ var _items: Array = []
 func get_item_list() -> Array:
     return _items
 
-# TODO Inventory size limits; this function should be able to fail if
-# we exceed inventory size limits.
-func add_item(item: ItemInstance) -> void:
-    _items.append(item)
+func get_item_count() -> int:
+    return len(_items)
+
+# Returns whether successful. This function will fail if the player's
+# inventory is full.
+func add_item(item: ItemInstance) -> bool:
+    if len(_items) < _max_items:
+        _items.append(item)
+        return true
+    else:
+        return false
 
 func remove_item(index: int) -> void:
     _items.remove(index)
